91355c7a196d563cbed3f1edece93b7de23f20cd
gonzagram94 <140217666+gonzagram94@users.noreply.github.com>
2023-08-08 22:17:19 -0300

    review final

--- Controllers/controladorReview.py
+++ Controllers/controladorReview.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 from Models.reviews import Review
 
+import tkinter as tk
+from Models.reviews import Review
+
+
 class ControladorReview:
     def __init__(self, app):
         self.app = app
@@ -10,44 +14,34 @@ class ControladorReview:
         self.app.cambiar_frame(self.app.vista_cargar_review)
 
     def seleccionar_destino_para_review(self, event):
-        indice_seleccionado = self.app.vista_cargar_review.destino_listbox.curselection()
-        indice_seleccionado =  indice_seleccionado[0]
-        destino_seleccionado= self.destinos[indice_seleccionado]
+        indice_seleccionado = (
+            self.app.vista_cargar_review.destino_listbox.curselection()
+        )
+        indice_seleccionado = indice_seleccionado[0]
+        destino_seleccionado = self.destinos[indice_seleccionado]
         self.app.vista_cargar_review.destino_seleccionado = destino_seleccionado
 
-        label_review = tk.Label(self.app.vista_cargar_review, text= f"Ingrese aquí su reseña del destino : {destino_seleccionado.nombre}")
-        label_review.pack()
-        #Creacion de entry para que el usuario ingrese su comentario
-        #entry_review = tk.Entry(self.app.vista_cargar_review)
-        #entry_review.pack()
+        self.app.vista_cargar_review.createReviewForm(destino_seleccionado)
 
-        # Crear el Text para que el usuario ingrese el comentario con dimensiones grandes para agregar varias lineas
-        text_review = tk.Text(self.app.vista_cargar_review, width=50, height=5)
-        text_review.pack()
-
-        # Almacenar el Text como un atributo del contenedor para acceder a él posteriormente
-        self.app.vista_cargar_review.text_review = text_review
-
-        # Almacenar el Entry como un atributo del contenedor para acceder a él posteriormente
-        #self.app.vista_cargar_review.entry_review = entry_review
-
-        # Crea el botón para guardar el comentario
-        boton_guardar = tk.Button(self.app.vista_cargar_review, text="Guardar", command=lambda: self.guardar_comentario(text_review.get("1.0", tk.END)))
-        boton_guardar.pack()
-        # DESCOMENTAR PARA LUEGO PONER LA CALIFICACION (DE 1 A 5)
-""" 
-    def guardar_comentario(self, comentario):
-        
-        reviewNueva = Review((self.app.destinos.len() + 1), self.app.vista_cargar_review.destino_seleccionado.id, 1,)
-        self.app.reviews.append()
-        # Guarda el comentario en una base de datos
-        print("Comentario guardado:")
-        print(comentario) 
-
-
-
-        print(indice)
-        if indice:
-            nombre_destino_seleccionado=self.listbox.get(indice[0])
-            destino_seleccionado=next((destino for destino in self))  """
-        
+    def cargar_review(self, review):
+        if review["calificacion"] < 3:
+            review["sentimiento"] = "Negativo"
+        # si review es neutro
+        elif review["calificacion"] == 3:
+            review["sentimiento"] = "Neutro"
+        else:
+            review["sentimiento"] = "Positivo"
+        reviewNueva = Review(
+            Review.generate_id("data/reviews.json"),
+            review["idDestino"],
+            1,  # cambiar por el id del usuario
+            review["calificacion"],
+            review["text"],
+            review["sentimiento"],
+        )
+        # guardo el json
+        reviewNueva.add_to_json("data/reviews.json")
+        # guardo la review en el contenedor
+        self.app.reviews.append(reviewNueva)
+        # # cambio a la vista de inicio
+        self.app.cambiar_frame(self.app.vista_inicio)
--- Views/createReview.py
+++ Views/createReview.py
@@ -19,8 +19,8 @@ class CreateReview(tk.Frame):
     def create_widgets(self):
         self.label_titulo = tk.Label(self, text="Reviews", font=("Arial", 20))
         self.label_titulo.pack(pady=10)
-        
-        ## TODO - descomentar , conectarlo a la base de datos y hacer que funcione  
+
+        ## TODO - descomentar , conectarlo a la base de datos y hacer que funcione
 
         """ # # listbox de posibles destinos"""
         self.label_destino = tk.Label(self, text="Destino")
@@ -32,33 +32,68 @@ class CreateReview(tk.Frame):
             self.destino_listbox.insert(tk.END, destino.nombre)
 
         self.destino_listbox.pack(pady=5)
-        self.destino_listbox.bind("<<ListboxSelect>>", self.controlador.seleccionar_destino_para_review)
-        
+        self.destino_listbox.bind(
+            "<<ListboxSelect>>", self.controlador.seleccionar_destino_para_review
+        )
 
-        for destino in self.destinos:
-             if destino.nombre == self.destino_listbox.get(tk.ACTIVE):
-                id_destino = destino.id
-        
-     # # listbox de posibles destinos"""
-        #self.label_review = tk.Label(self, text="Ingrese aquí su reseña de: ")
-        #self.label_review.pack(pady=5)
+        boton_volver = tk.Button(
+            self,
+            text="Volver",
+            command=lambda: self.app.cambiar_frame(self.app.vista_inicio),
+        )
+        boton_volver.pack()
 
+    def createReviewForm(self, destino):
+        if hasattr(self.app.vista_cargar_review, "labelFormTitle"):
+            self.app.vista_cargar_review.labelFormTitle.destroy()
+            self.app.vista_cargar_review.text_review.destroy()
+            self.app.vista_cargar_review.label_calificacion.destroy()
+            self.app.vista_cargar_review.listbox_calificacion.destroy()
+            self.app.vista_cargar_review.boton_cargar_review.destroy()
+            self.app.vista_cargar_review.boton_volver.destroy()
 
+        self.app.vista_cargar_review.labelFormTitle = tk.Label(
+            self, text="Agrege su reseña del destino : " + destino.nombre + " :"
+        )
+        self.app.vista_cargar_review.labelFormTitle.pack(pady=10)
+        self.app.vista_cargar_review.text_review = tk.Text(self, width=50, height=1)
+        self.app.vista_cargar_review.text_review.pack(pady=5)
+        self.app.vista_cargar_review.label_calificacion = tk.Label(
+            self, text="Calificacion"
+        )
+        self.app.vista_cargar_review.label_calificacion.pack(pady=5)
+        self.app.vista_cargar_review.listbox_calificacion = tk.Listbox(
+            self, width=2, height=1
+        )
+        self.app.vista_cargar_review.listbox_calificacion.pack(pady=5)
+        for calificacion in range(1, 6):
+            self.app.vista_cargar_review.listbox_calificacion.insert(
+                tk.END, calificacion
+            )
 
-        """# # listbox de posibles usuarios
-""" 
-"""         self.label_usuario = tk.Label(self, text="Usuario")
+        self.app.vista_cargar_review.listbox_calificacion.pack(pady=5)
 
-        self.label_usuario.pack(pady=5)
+        self.app.vista_cargar_review.boton_cargar_review = tk.Button(
+            self,
+            text="Cargar Review",
+            command=lambda: self.controlador.cargar_review(
+                {
+                    "nameDestino": destino.nombre,
+                    "idDestino": destino.id,
+                    "text": self.app.vista_cargar_review.text_review.get("1.0", tk.END),
+                    "calificacion": self.app.vista_cargar_review.listbox_calificacion.get(
+                        tk.ACTIVE
+                    ),
+                }
+            ),
+        )
 
-        self.usuario_listbox = tk.Listbox(self, width=80, height=20)
+        self.app.vista_cargar_review.boton_cargar_review.pack()
 
-        for usuario in self.usuarios:
-            self.usuario_listbox.insert(tk.END, usuario.nombre, usuario.apellido)
-
-        self.usuario_listbox.pack(pady=5)
-
-        for usuario in self.usuarios:
-            if usuario.nombre == self.usuario_listbox.get(tk.ACTIVE):
-                id_usuario = usuario.id """ """
- """ 
+        # volver atras
+        self.app.vista_cargar_review.boton_volver = tk.Button(
+            self,
+            text="Volver",
+            command=lambda: self.app.cambiar_frame(self.app.vista_inicio),
+        )
+        self.app.vista_cargar_review.boton_volver.pack()
